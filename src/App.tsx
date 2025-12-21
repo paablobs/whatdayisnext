9e65547a00499a37db90492bd2d23ea2f54edeb5
Pablo Bessone <pablo.bessone@outlook.com>
2025-12-21 04:57:10 +0000

    dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { ColorModeProvider } from "@/components/ui/color-mode";
 import MainView from "./components/MainView/MainView";
 import { system } from "./theme";
 
 function App() {
   return (
     <ChakraProvider value={system}>
-      <MainView />
+      <ColorModeProvider forcedTheme="dark">
+        <MainView />
+      </ColorModeProvider>
     </ChakraProvider>
   );
 }
